f7ea73b996b8229035953f4a4da164833abce116
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-01-18 21:32:40 +0200

    init

--- SwiftUIEssentialsColor/ContentView.swift
+++ SwiftUIEssentialsColor/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Circle()
+            .padding()
+            .foregroundColor(Color(.magenta).opacity(0.5))
+                
+            .background(Color(.systemBlue))
     }
 }
 
